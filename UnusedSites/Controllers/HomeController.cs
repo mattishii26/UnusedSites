87d1a9d59e078a03e1c51898e414a6fcd15261d2
Matthew Ishii <chihiro2657@gmail.com>
2018-12-17 00:59:10 -0800

    Add project files.

---
+++ UnusedSites/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UnusedSites.Models;
+
+namespace UnusedSites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            ViewData["Title"] = "Home";
+            return View();
+        }
+
+        public IActionResult AccountHome()
+        {
+            ViewData["Title"] = "Account Home";
+            return View();
+        }
+
+        public IActionResult AddSite()
+        {
+            ViewData["Title"] = "Add an Unused Site";
+            return View();
+        }
+
+    }
+}
---
+++ UnusedSites/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UnusedSites.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
