7a0f7814e6ac80a61a91df4d30123ffccf1fb276
Allee <jdds638@gmail.com>
2019-02-16 13:48:41 -0800

    init api start

---
+++ UnusedSites/Api/DbConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace UnusedSites.Api
+{
+    public class DbConnection
+    {
+        private static IConfigurationRoot Configuration;
+
+        public static string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("websettings.json");
+
+            Configuration = builder.Build();
+            var connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            return connectionString;
+        }
+    }
+}
---
+++ UnusedSites/Api/District.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnusedSites.Api
+{
+    public class District
+    {
+        private string conString;
+
+        public District()
+        {
+            conString = DbConnection.GetConnectionString();
+        }
+
+
+    }
+}
