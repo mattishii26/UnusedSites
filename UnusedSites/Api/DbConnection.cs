14e7c32146e12996fd2aa12ddcda57fc17c4c6fe
Allee <jdds638@gmail.com>
2019-02-16 18:18:07 -0800

    query db to get all districts

--- UnusedSites/Api/DbConnection.cs
+++ UnusedSites/Api/DbConnection.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
 using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace UnusedSites.Api
 {
@@ -20,6 +22,32 @@ namespace UnusedSites.Api
             Configuration = builder.Build();
             var connectionString = Configuration["ConnectionStrings:DefaultConnection"];
             return connectionString;
+        }
+
+        public static DataTable SelectQuery(string query)
+        {
+            DataTable temp;
+
+            using (SqlConnection con = new SqlConnection(GetConnectionString()))
+            {
+                con.Open();
+
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = @query;
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataSet ds = new DataSet();
+
+                        da.Fill(ds, "tablename");
+                        con.Close();
+
+                        temp = ds.Tables["tablename"];
+                    }
+                }
+            }
+            return temp;
         }
     }
 }
--- UnusedSites/Api/District.cs
+++ UnusedSites/Api/District.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace UnusedSites.Api
 {
@@ -14,6 +15,13 @@ namespace UnusedSites.Api
             conString = DbConnection.GetConnectionString();
         }
 
-
+        public static DataTable GetAllDistricts()
+        {
+            string query = "SELECT dist.dist_name, un.contact_salutation, un.contact_fname, un.contact_lname, un.contact_phone," +
+                "un.date_sent, un.date_received, un.form424_rec_date" +
+                "FROM db.unused_dist as un, db.district as dist" +
+                "WHERE dist.dist_code = un.dist_code;";
+            return DbConnection.SelectQuery(query);
+        }
     }
 }
