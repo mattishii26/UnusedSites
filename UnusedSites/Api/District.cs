cda2d604c0996061d8ae7c794f8102f68f3ffae5
Allee <jdds638@gmail.com>
2019-02-22 15:53:38 -0800

    get all districts and create a site

--- UnusedSites/Api/District.cs
+++ UnusedSites/Api/District.cs
@@ -8,20 +8,15 @@ namespace UnusedSites.Api
 {
     public class District
     {
-        private string conString;
-
-        public District()
-        {
-            conString = DbConnection.GetConnectionString();
-        }
 
         public static DataTable GetAllDistricts()
         {
-            string query = "SELECT dist.dist_name, un.contact_salutation, un.contact_fname, un.contact_lname, un.contact_phone," +
-                "un.date_sent, un.date_received, un.form424_rec_date" +
-                "FROM db.unused_dist as un, db.district as dist" +
-                "WHERE dist.dist_code = un.dist_code;";
-            return DbConnection.SelectQuery(query);
+            string query = @"SELECT dist.dist_name, un.contact_salutation, un.contact_fname, un.contact_lname, un.contact_phone,
+                un.date_sent, un.date_received, un.form424_rec_date
+                FROM db.unused_dist as un, db.district as dist
+                WHERE dist.dist_code = un.dist_code;";
+            return DbConnection.SelectQuery(query, new List<object>());
         }
+
     }
 }
---
+++ UnusedSites/Api/Site.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnusedSites.Api
+{
+    public class Site
+    {
+
+        public static Boolean CreateSite(string districtCode, string coCode, string siteName, string status, double numAcres, int monthLastUsed, int yearLastUsed,
+            double purchasePrice, double appraisedValue, int monthAppraised, int yearAppraised, double currentValue, string gradeLevel)
+        {
+            string siteQuery = @"insert into db.site (dist_code, co_code, site_name, site_code, Status)
+                            values(@p0, @p1, @p2, @p3, @p4, @p5)
+                            ;";
+            List<object> siteParam = new List<object>();
+            siteParam.Add(districtCode);
+            siteParam.Add(coCode);
+            siteParam.Add(siteName);
+            siteParam.Add(12345); // this is currently hard coded, will need to be updated when i ask how it is populated
+            siteParam.Add(status);
+            int siteRows = DbConnection.CUDQuery(siteQuery, siteParam);
+
+            string unusedQuery = @"insert into db.unusedvb (dist_code, co_code, site_code, num_acres, mo_last_used, yr_last_used, purch_price, appraised_value,
+                               mo_appraised, yr_appraised, current_value, grade_level)
+                               values(@p0, @p1, @p2, @p3, @p4, @p5, @p6, @p7, @p8, @p9, @p10, @p11)
+                               ;";
+            List<object> unusedParam = new List<object>();
+            unusedParam.Add(districtCode);
+            unusedParam.Add(coCode);
+            unusedParam.Add(12345); // this is currently hard coded, will need to be updated when i ask how it is populated
+            unusedParam.Add(numAcres);
+            unusedParam.Add(monthLastUsed);
+            unusedParam.Add(yearLastUsed);
+            unusedParam.Add(purchasePrice);
+            unusedParam.Add(appraisedValue);
+            unusedParam.Add(monthAppraised);
+            unusedParam.Add(yearAppraised);
+            unusedParam.Add(currentValue);
+            unusedParam.Add(gradeLevel);
+            int unusedRows = DbConnection.CUDQuery(unusedQuery, unusedParam);
+
+            if (unusedRows > 0 && siteRows > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
