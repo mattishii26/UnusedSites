c735dc23747962212ad546599683057f83155c4a
mattishii26 <chihiro2657@gmail.com>
2019-04-25 00:20:30 -0700

    Changes to Add a Site prior to adding input fields.

--- UnusedSites/wwwroot/js/custom.js
+++ UnusedSites/wwwroot/js/custom.js
@@ -10,6 +10,26 @@ function waiverHandler(c, idSec) {
 }
 
 function test(c) {
-    alert(c.checked
-        );
+    alert(c.checked);
+}
+
+function deleteFormHandler() {
+    $("#soldDelete").addClass("hidden");
+    $("#purposeDelete").addClass("hidden");
+    $("#constructDelete").addClass("hidden");
+    $("#" + $("#deleteReason").val()).removeClass("hidden");
+}
+
+function waiveFormHandler() {
+    $("#usedWaive").addClass("hidden");
+    $("#sellWaive").addClass("hidden");
+    $("#leaseWaive").addClass("hidden");
+    $("#utilizeWaive").addClass("hidden");
+    $("#" + $("#waiveReason").val()).removeClass("hidden");
+}
+
+function reduceFormHandler() {
+    $("#debtReduce").addClass("hidden");
+    $("#modReduce").addClass("hidden");
+    $("#" + $("#reduceReason").val()).removeClass("hidden");
 }
